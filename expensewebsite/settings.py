c87a1a9c620a77d125675dc6b36a28fff6c20845
dubhe-lin <dubhelinn@gmail.com>
2025-01-09 12:18:27 +0700

    Add expensesweb.onrender.com to ALLOWED_HOSTS

--- expensewebsite/settings.py
+++ expensewebsite/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-c&_n4(*vndjmd#v+e=o0^y@wk89yfnfc6^&5p636nwy($(+z#w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['expensesweb.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
